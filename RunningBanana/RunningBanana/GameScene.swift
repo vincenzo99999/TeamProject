82f18aa9aba0a90bed813bf5941f14d75dbba60e
Arthur <41858848+ArturoHaru@users.noreply.github.com>
2023-12-08 21:10:35 +0100

    Parallax Implemented

--- RunningBanana/RunningBanana/GameScene.swift
+++ RunningBanana/RunningBanana/GameScene.swift
@@ -10,100 +10,49 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
+    //Parallax Array
+    var parallaxLayerSprites: [SKSpriteNode?]? = []
+    var parallax: Parallax? = nil
     
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    
+    var startTime: TimeInterval? = nil
+    var hasStartTimeBeenAssigned = false
+    
+    
     
     override func sceneDidLoad() {
-
-        self.lastUpdateTime = 0
+        self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        //get layers, somewhere
+        let backTreeSprite = childNode(withName: "backTreeSprite") as! SKSpriteNode?
+        let middleTreeSprite = childNode(withName: "middleTreeSprite") as! SKSpriteNode?
+        let frontTreeSprite = childNode(withName: "frontTreeSprite") as! SKSpriteNode?
+        let lightSprite = childNode(withName: "lightSprite") as! SKSpriteNode?
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+        //Append layers in order from the furthest one to the closest
+        parallaxLayerSprites?.append(backTreeSprite)
+        parallaxLayerSprites?.append(middleTreeSprite)
+        parallaxLayerSprites?.append(lightSprite)
+        parallaxLayerSprites?.append(frontTreeSprite)
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        parallax = Parallax(scene: self, parallaxLayerSprites: parallaxLayerSprites!, getSpritesFromFile: false)
     }
     
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-        
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
+        //Get start time
+        if !hasStartTimeBeenAssigned {
+            startTime = currentTime
+            hasStartTimeBeenAssigned.toggle()
         }
         
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
-        self.lastUpdateTime = currentTime
+        parallax!.update(layers: parallaxLayerSprites!, speed: 0.5, speedFactor: 1.0);
     }
+    
+    func elapsedTime(currentTime: TimeInterval) -> TimeInterval{
+        return currentTime - startTime!
+    }
+    
+
 }
