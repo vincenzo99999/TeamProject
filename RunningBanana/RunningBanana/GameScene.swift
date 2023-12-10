6da81ef7bd25444a0689e6d5559002b3e6e72fb6
vincenzo99999 <150272586+vincenzo99999@users.noreply.github.com>
2023-12-10 14:18:18 +0100

    fixed collision between the player and the watermelon,added gameOver conditions,implemented contacts via bitMasks witht the ground and the obstacles

--- RunningBanana/RunningBanana/GameScene.swift
+++ RunningBanana/RunningBanana/GameScene.swift
@@ -7,7 +7,13 @@
 
 import SpriteKit
 import GameplayKit
-
+struct PhysicsCategory{
+    static let none:UInt32 = 0
+    static let all :UInt32 = UInt32.max
+    static let player : UInt32 = 0b1
+    static let watermelon : UInt32 = 0b10
+    static let floor:UInt32=0b11
+}
 protocol FloorContactDelegate: AnyObject {
     func playerDidContactFloor()
 }
@@ -72,27 +78,37 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         updateWatermelonPosition()
         jump()
         updateScore()
-
+        if GameOver(){
+            //add gameOverView()
+        }
     }
-    
+    func GameOver()->Bool{
+        if player.position.x<=frame.width||player.position.y<floor.position.y{
+            return true
+        }
+        else{
+            return false
+        }
+    }
     func elapsedTime(currentTime: TimeInterval) -> TimeInterval{
         return currentTime - startTime!
     }
     func updateScore(){
         score=score+((player.physicsBody?.velocity.dx)! + CGFloat(obstacleCounter)) * 0.001
         scoreLabel.text = "\(Int(score))"
-        print(score)
     }
     func createPlayer() {
         self.player = SKSpriteNode(color: .blue, size: CGSize(width: 50, height: 50))
         player.name = "player"
-        player.physicsBody = SKPhysicsBody(circleOfRadius: 25)
+        player.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 50))
         player.position = CGPoint(x: -50, y: 35)
-        player.physicsBody?.affectedByGravity=true
-        player.physicsBody?.categoryBitMask = 1
-        player.physicsBody?.contactTestBitMask = 1
-        player.zPosition=10
-        player.physicsBody?.allowsRotation=false
+        player.physicsBody?.affectedByGravity = true
+        player.physicsBody?.categoryBitMask = PhysicsCategory.player
+        player.physicsBody?.contactTestBitMask = PhysicsCategory.watermelon
+        player.physicsBody?.collisionBitMask = PhysicsCategory.watermelon | PhysicsCategory.floor
+        player.physicsBody?.allowsRotation = false
+        player.physicsBody?.velocity.dy = 0
+        player.zPosition = 10
         addChild(player)
     }
     func createFloor() {
@@ -100,7 +116,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         floor.position = CGPoint(x: 0, y: -50)  // Imposta la posizione del pavimento
         floor.physicsBody = SKPhysicsBody(rectangleOf: floor.size)
         floor.physicsBody?.affectedByGravity=false
-        floor.physicsBody?.categoryBitMask = 2
+        floor.physicsBody?.categoryBitMask = 2|PhysicsCategory.floor
+        floor.physicsBody?.categoryBitMask = PhysicsCategory.floor
+        floor.physicsBody?.contactTestBitMask = PhysicsCategory.player
+        floor.physicsBody?.collisionBitMask = PhysicsCategory.player
         floor.physicsBody?.isDynamic = false  // Il pavimento non si muove
         floor.zPosition=9
         addChild(floor)
@@ -108,19 +127,24 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     func createWatermelon(){
         watermelon=SKSpriteNode(color: .green, size: CGSize(width: 25, height: 25))
         watermelon.position.x=70
-        watermelon.position.y=player.position.y
+        watermelon.position.y=CGFloat.random(in:floor.position.y..<frame.height)
+        watermelon.name="Watermelon"
         watermelon.physicsBody=SKPhysicsBody(circleOfRadius:25)
         watermelon.physicsBody?.affectedByGravity=false
         watermelon.physicsBody?.categoryBitMask=4
         watermelon.physicsBody?.contactTestBitMask = 4
-        watermelon.physicsBody?.isDynamic=false
+        watermelon.physicsBody?.isDynamic=true
         watermelon.zPosition=11
+        watermelon.physicsBody?.categoryBitMask = PhysicsCategory.watermelon
+        watermelon.physicsBody?.contactTestBitMask = PhysicsCategory.player
+        watermelon.physicsBody?.collisionBitMask = PhysicsCategory.player
+        
         addChild(watermelon)
         
     }
     func updateWatermelonPosition(){
         watermelon.position.x -= 1.4
-        if watermelon.position.x < -150{
+        if watermelon.position.x < -80{
             if let node=watermelon{
                 node.removeFromParent()
                 createWatermelon()
@@ -187,23 +211,28 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         scoreLabel.zPosition=7
         addChild(scoreLabel)
     }
-    func didBegin(_ contact: SKPhysicsContact) {
-        let contactMask = contact.bodyA.categoryBitMask | contact.bodyB.categoryBitMask
-        
-        // Verifica se il giocatore ha contattato l'acqua (watermelon)
-        if contactMask == 5 {  // 1 (giocatore) | 4 (acqua)
-            if let playerNode = contact.bodyA.categoryBitMask == 1 ? contact.bodyA.node : contact.bodyB.node,
-               let watermelonNode = contact.bodyA.categoryBitMask == 4 ? contact.bodyA.node : contact.bodyB.node {
-                watermelonNode.removeFromParent()
-                createWatermelon()
-            }
+    public func didBegin(_ contact: SKPhysicsContact) {
+        let firstBody: SKPhysicsBody = contact.bodyA
+        let secondBody: SKPhysicsBody = contact.bodyB
+
+        if let node = firstBody.node, node.name == "Watermelon" {
+            node.removeFromParent()
+            createWatermelon()
+            print("contatto rilevato")
+        }
+        if let node = secondBody.node, node.name == "Watermelon" {
+            node.removeFromParent()
+            createWatermelon()
+            print("contatto rilevato")
         }
     }
+
 
     override func didMove(to view: SKView) {
         physicsWorld.contactDelegate = self
     }
 }
+
 
         
 
