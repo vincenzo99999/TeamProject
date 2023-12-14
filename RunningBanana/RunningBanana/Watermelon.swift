cb884cb850cffe928e4de0e9adecbff00c996f0e
Arthur <41858848+ArturoHaru@users.noreply.github.com>
2023-12-14 22:39:28 +0100

    Fixed object spawning underground

--- RunningBanana/RunningBanana/Watermelon.swift
+++ RunningBanana/RunningBanana/Watermelon.swift
@@ -13,7 +13,7 @@ class Watermelon: Spawnable{
     override func Respawn() {
         
         let horizontalPosition = (scene.frame.width) + sprite.size.width
-        let elevation = CGFloat.random(in: -(floorHeight + 50.0)..<((scene.size.height)/5))
+        let elevation = CGFloat.random(in: (-floorHeight + 150.0)..<((scene.size.height)/5))
 
         
         self.spawn(spawnPosition: CGPoint(x: horizontalPosition,y: elevation))
