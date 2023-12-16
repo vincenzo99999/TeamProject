1d3205daa6034d7ed61c1d58666671b8a56b3542
Giovanni <giova.bifulco@gmail.com>
2023-12-16 16:45:10 +0100

    Added tutorial
    tutorial

--- RunningBanana/RunningBanana/TutorialScene.swift
+++ RunningBanana/RunningBanana/TutorialScene.swift
@@ -13,9 +13,9 @@ import Foundation
 class TutorialScene: SKScene {
     
     override func didMove(to view: SKView) {
-        backgroundColor = SKColor.white // Set a background color
+        backgroundColor = SKColor.white
         
-        // Title
+        // Titolo
         let titleLabel = SKLabelNode(text: "Tutorial")
         titleLabel.fontColor = .black
         titleLabel.fontName = "Helvetica-Bold"
@@ -23,16 +23,15 @@ class TutorialScene: SKScene {
         titleLabel.position = CGPoint(x: frame.midX, y: frame.midY + 150)
         addChild(titleLabel)
         
-        // Description
         let descriptionText = [
-            "Tony is in trouble! He has to recover his watermelons to sell them at the Market.",
-            "Help him gather as many watermelons as possible!",
+            "Tony is in trouble! Help him gather as many watermelons as possible!",
+            "Tap the screen to make the Trerote jump",
             "You can use the Gas tanks to go a bit faster.",
-            "Every watermelon you pick, higher the score multiplier will be!"
-        ]
+            "Every watermelon you collect will increase the score multiplier!"
+            ]
         
-        var currentYPosition = frame.midY + 70
-        let spacing: CGFloat = 50.0
+        var currentYPosition = frame.midY + 60
+        let spacing: CGFloat = 60.0
         
         for line in descriptionText {
             let lineLabel = SKLabelNode(text: line)
@@ -40,8 +39,8 @@ class TutorialScene: SKScene {
             lineLabel.fontName = "Helvetica-Bold"
             lineLabel.fontSize = 30.0
             lineLabel.position = CGPoint(x: frame.midX, y: currentYPosition)
-            lineLabel.numberOfLines = 8 // Allow multiple lines
-            lineLabel.preferredMaxLayoutWidth = frame.width - 50 // Set max line width
+            lineLabel.numberOfLines = 8
+            lineLabel.preferredMaxLayoutWidth = frame.width - 50
             addChild(lineLabel)
             currentYPosition -= spacing
         }
@@ -50,7 +49,7 @@ class TutorialScene: SKScene {
         backButton.fontColor = .black
         backButton.fontName = "Helvetica-Bold"
         backButton.fontSize = 30.0
-        backButton.position = CGPoint(x: frame.midX, y: frame.minY + 50) // Adjust position as needed
+        backButton.position = CGPoint(x: frame.midX, y: frame.minY + 50)
         backButton.name = "backButton"
         addChild(backButton)
     }
