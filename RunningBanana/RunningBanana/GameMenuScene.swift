5e91e44dce96854968b31f7a4d80c75797b2ed1d
Giovanni <giova.bifulco@gmail.com>
2023-12-18 16:08:42 +0100

    アペカーラン

--- RunningBanana/RunningBanana/GameMenuScene.swift
+++ RunningBanana/RunningBanana/GameMenuScene.swift
@@ -45,7 +45,7 @@ class GameMenuScene: SKScene {
     }
 
     func setupUI() {
-        let titleLabel = SKLabelNode(text: "Three Wheels")
+        let titleLabel = SKLabelNode(text: "アペカーラン")
         titleLabel.fontColor = .black
         titleLabel.fontName="helvetica-bold"
         titleLabel.fontSize = 50.0
